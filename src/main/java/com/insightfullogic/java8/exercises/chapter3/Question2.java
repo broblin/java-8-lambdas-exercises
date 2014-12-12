8a912663c15efd86e21668a60e86fa118eb74fda
broblin <benoit.roblin@wonderbox.fr>
2014-12-12 13:57:24 +0100

    résolution de l'exercice 2
    réponse question 3 :
    a) eager
    b) lazy
    
    réponse question 4:
    a) oui (fonction en paramètre)
    b) non

--- src/main/java/com/insightfullogic/java8/exercises/chapter3/Question2.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter3/Question2.java
@@ -3,11 +3,32 @@ package com.insightfullogic.java8.exercises.chapter3;
 import com.insightfullogic.java8.examples.chapter1.Artist;
 import com.insightfullogic.java8.exercises.Exercises;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Question2 {
     // Q3
     public static int countBandMembersInternal(List<Artist> artists) {
-        return Exercises.replaceThisWithSolution();
+        return (int) artists.stream().mapToLong(artist -> artist.getMembers().count()).reduce((a,b) -> a+b).getAsLong();
+    }
+
+    public static void main(String[] args){
+        List<Artist> artistes = new ArrayList<>();
+        List<Artist> members = new ArrayList<>();
+        List<Artist> members2 = new ArrayList<>();
+
+        members.add(new Artist("Bruce Dickinson","Angleterre"));
+        members.add(new Artist("Steve Harris","Angleterre"));
+        members.add(new Artist("Dave Murray","Angleterre"));
+
+        members2.add(new Artist("Joseph Duplantier","France"));
+        members2.add(new Artist("Mario Duplantier","France"));
+
+        Artist artist1 = new Artist("Iron Maiden",members," Angleterre");
+        Artist artist2 = new Artist("Gojira",members2,"France");
+
+        artistes.add(artist1);
+        artistes.add(artist2);
+        System.out.println(countBandMembersInternal(artistes));
     }
 }
