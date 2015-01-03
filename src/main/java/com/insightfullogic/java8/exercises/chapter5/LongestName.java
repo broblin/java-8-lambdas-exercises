ff0a254bc5f34af231b72e0797ff609c1d47d452
benoit <benoit.roblin@yahoo.fr>
2015-01-03 12:14:40 +0100

    exercice LongestName du chapitre 5. avec ajout de commentaires sur les optional.

--- src/main/java/com/insightfullogic/java8/exercises/chapter5/LongestName.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter5/LongestName.java
@@ -1,23 +1,23 @@
 package com.insightfullogic.java8.exercises.chapter5;
 
 import com.insightfullogic.java8.examples.chapter1.Artist;
-import com.insightfullogic.java8.exercises.Exercises;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
+import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
-
-import static java.util.Comparator.comparing;
 
 public class LongestName {
 
     public static Artist byReduce(List<Artist> artists) {
-        return Exercises.replaceThisWithSolution();
+        //si la valeur est absente (liste vide) lève une exception de type NoSuchElementException
+        return artists.stream().reduce(BinaryOperator.maxBy(Comparator.comparingInt(a -> a.getName().length()))).get();
     }
 
     public static Artist byCollecting(List<Artist> artists) {
-        return Exercises.replaceThisWithSolution();
+        Comparator<Artist> nameByLengthComparator = Comparator.comparingInt(a -> a.getName().length());
+        //le collect retourne un optional, la méthode orElseThrow  lève une exception (orElseGet existe aussi ...)
+        return artists.stream().collect(Collectors.maxBy(nameByLengthComparator)).orElseThrow(RuntimeException::new);
     }
 
 }
