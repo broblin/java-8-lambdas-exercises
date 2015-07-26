13f6dc148e3df81c144a8dcce9f9defea4090a61
benoit roblin <benoit@MacBook-Pro-de-benoit.local>
2015-07-26 21:50:00 +0200

    revision lambda + functional programming

---
+++ src/main/java/com/revision/functionalExercices/Livre.java
@@ -0,0 +1,7 @@
+package com.revision.functionalExercices;
+
+/**
+ * Created by benoit on 26/07/15.
+ */
+public class Livre {
+}
---
+++ src/main/java/com/revision/operatorExercice/IBinaryExpression.java
@@ -0,0 +1,8 @@
+package com.revision.operatorExercice;
+
+/**
+ * Created by benoit on 26/07/15.
+ */
+public interface IBinaryExpression {
+    Long executeBinaryExpression(long left,long right);
+}
